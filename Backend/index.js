1f5844c96ce4675c657d7ce9955b0b43ceb8977e
Indresh Maurya <indreshmauryakvs@gmail.com>
2025-05-19 15:07:57 +0530

    final deploy code

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,8 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from "path";
+
 dotenv.config({});
 const app = express();
 //middleware
@@ -24,6 +26,17 @@ app.use("/api/user", userRoute);
 app.use("/api/company", companyRoute);
 app.use("/api/job", jobRoute);
 app.use("/api/application", applicationRoute);
+
+// -----------Code for deployment-------------------
+if (process.env.NODE_ENV === "production") {
+ const dirpath = path.resolve();
+  app.use(express.static('./Frontend/dist'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(dirpath, "./Frontend/dist" , 'index.html' ));
+  });
+}
+
+
 
 app.listen(PORT, () => {
   connectDB();
